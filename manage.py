78cae4f1cccb04c93b565d849d178d4652f27fcf
emilija-maz <emilijamazuraite@gmail.com>
2019-04-04 19:23:36 +0100

    changes with instructors

--- manage.py
+++ manage.py
@@ -1,10 +1,39 @@
-#!/usr/bin/env python
-import os
-import sys
+from flask import Flask, render_template, request
+import requests
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+app = Flask("MyApp")
 
-    from django.core.management import execute_from_command_line
+@app.route("/")
+def hello():
+	return "Hello World"
 
-    execute_from_command_line(sys.argv)
+@app.route("/<name>")
+def hello_name(name):
+	return render_template("landingpage.html", name = name.title())
+	# by default it checks templates folder
+	# if name = None => it will say hello anonymous person
+
+@app.route("/")
+def serve_index():
+	return render_template("email.html")
+
+
+@app.route("/email", methods=["POST"])
+def email():
+	form_data = request.form
+	print form_data["email"]
+	requests.post(
+		"https://api.mailgun.net/v3/sandboxf8a60abf48434795bd9e0fee08a8494f.mailgun.org/messages",
+        auth=("api", "f901c98b1d9032490412503076633c44-acb0b40c-e4f0c684"),
+        data={"from": " Your past self <mailgun@sandboxf8a60abf48434795bd9e0fee08a8494f.mailgun.org>",
+              "to": ["emilijamazuraite@gmail.com"],
+              "subject": "This is a test, don't panic",
+              "text": "Hi" +  form_data["name"] +"I am testing a flask file!"}
+		)
+              # "Hi {}, this is an email".format(form_data["name"])
+	return "All OK cool"
+
+
+
+app.run(debug=True)
+
